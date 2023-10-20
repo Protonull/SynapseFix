8c72c15cea237cdd9d9c4be6f7a716e99fa3a883
Alexander <protonull@protonmail.com>
2023-10-21 00:45:42 +0100

    Add screenshot privacy mixin
    While screenshots are *currently* not being scraped, remote-code execution being what it is, that could change at any point.

---
+++ common/src/main/java/uk/protonull/synapsefix/common/mixins/ScreenshotPrivacyMixin.java
@@ -0,0 +1,26 @@
+package uk.protonull.synapsefix.common.mixins;
+
+import com.mojang.blaze3d.pipeline.RenderTarget;
+import gjum.minecraft.civ.synapse.common.SynapseMod;
+import java.io.File;
+import java.util.function.Consumer;
+import net.minecraft.network.chat.Component;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(value = SynapseMod.class, remap = false)
+public class ScreenshotPrivacyMixin {
+    @Inject(method = "handleGrabScreenshot", at = @At("HEAD"), cancellable = true)
+    public void INJECT_handleGrabScreenshot(
+            final File file,
+            final @Nullable String string,
+            final RenderTarget renderTarget,
+            final Consumer<Component> consumer,
+            final CallbackInfoReturnable<Boolean> cir
+    ) {
+        cir.setReturnValue(false);
+    }
+}
